950777e87bf832f4e0211cd58c48227c1d76c264
Tong Leiming <tongleiming@daling.com>
2020-03-24 12:29:51 +0800

    增加注释

--- fishhookTest/fishhook/fishhook.c
+++ fishhookTest/fishhook/fishhook.c
@@ -60,6 +60,7 @@ static struct rebindings_entry *_rebindings_head;
 static int prepend_rebindings(struct rebindings_entry **rebindings_head,
                               struct rebinding rebindings[],
                               size_t nel) {
+  // rebindings_entry是一个单链表结构体，链表中的每个节点的内容就是本次替换的rebinding的一个数组；
   struct rebindings_entry *new_entry = (struct rebindings_entry *) malloc(sizeof(struct rebindings_entry));
   if (!new_entry) {
     return -1;
@@ -71,6 +72,7 @@ static int prepend_rebindings(struct rebindings_entry **rebindings_head,
   }
   memcpy(new_entry->rebindings, rebindings, sizeof(struct rebinding) * nel);
   new_entry->rebindings_nel = nel;
+  // rebindings_entry单链表是从头插入的，每次插入之后更新链表头
   new_entry->next = *rebindings_head;
   *rebindings_head = new_entry;
   return 0;
@@ -82,26 +84,37 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
                                            nlist_t *symtab,
                                            char *strtab,
                                            uint32_t *indirect_symtab) {
+  // 在间接跳转符号表中的偏移，定位到相关section位置处
   uint32_t *indirect_symbol_indices = indirect_symtab + section->reserved1;
+  // 计算section的地址，ASLR+vmaddress，section是一个指针数组，相当于得到了数组地址
   void **indirect_symbol_bindings = (void **)((uintptr_t)slide + section->addr);
   for (uint i = 0; i < section->size / sizeof(void *); i++) {
+    // 从间接符号表中获取在符号表中的索引，间接符号表是一个符号表索引的数组
+    //【*section这个指针数组与"此时的"indirect_symbol_indices数组下标是一一对应的*】
     uint32_t symtab_index = indirect_symbol_indices[i];
     if (symtab_index == INDIRECT_SYMBOL_ABS || symtab_index == INDIRECT_SYMBOL_LOCAL ||
         symtab_index == (INDIRECT_SYMBOL_LOCAL   | INDIRECT_SYMBOL_ABS)) {
       continue;
     }
+    // 获得字符串表的索引
     uint32_t strtab_offset = symtab[symtab_index].n_un.n_strx;
+    // 获取符号真正的name
     char *symbol_name = strtab + strtab_offset;
+    // 判断函数的名称字符大于1，因为函数前边有个_，所以方法名称最少要1个
     bool symbol_name_longer_than_1 = symbol_name[0] && symbol_name[1];
     struct rebindings_entry *cur = rebindings;
     while (cur) {
       for (uint j = 0; j < cur->rebindings_nel; j++) {
+        // 符号name大于1，并且抛去开头"_"之后的字符串与我们要供的name相等
         if (symbol_name_longer_than_1 &&
             strcmp(&symbol_name[1], cur->rebindings[j].name) == 0) {
+          // 替换的函数不为空，并且替换的函数和原来的函数不一样时
           if (cur->rebindings[j].replaced != NULL &&
               indirect_symbol_bindings[i] != cur->rebindings[j].replacement) {
+            // 让rebindings[j].replaced保存原有的indirect_symbol_bindings[i]的函数地址
             *(cur->rebindings[j].replaced) = indirect_symbol_bindings[i];
           }
+          // 将替换后的方法写入section对应的索引下边
           indirect_symbol_bindings[i] = cur->rebindings[j].replacement;
           goto symbol_loop;
         }
@@ -112,6 +125,8 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
   }
 }
 
+// 该函数首先获取__LINKEDIT，符号表(LC_SYMTAB)，间接跳转表，字符串表在内存中的位置，
+// 然后调用perform_rebinding_with_section分别对懒加载表和非懒加载表进行替换。
 static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
                                      const struct mach_header *header,
                                      intptr_t slide) {
@@ -125,15 +140,19 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
   struct symtab_command* symtab_cmd = NULL;
   struct dysymtab_command* dysymtab_cmd = NULL;
 
+  // 跨过Mach Header内容
   uintptr_t cur = (uintptr_t)header + sizeof(mach_header_t);
   for (uint i = 0; i < header->ncmds; i++, cur += cur_seg_cmd->cmdsize) {
     cur_seg_cmd = (segment_command_t *)cur;
+    // 获取__LINKEDIT
     if (cur_seg_cmd->cmd == LC_SEGMENT_ARCH_DEPENDENT) {
       if (strcmp(cur_seg_cmd->segname, SEG_LINKEDIT) == 0) {
         linkedit_segment = cur_seg_cmd;
       }
+    // 获取符号表
     } else if (cur_seg_cmd->cmd == LC_SYMTAB) {
       symtab_cmd = (struct symtab_command*)cur_seg_cmd;
+    // 获取动态符号表
     } else if (cur_seg_cmd->cmd == LC_DYSYMTAB) {
       dysymtab_cmd = (struct dysymtab_command*)cur_seg_cmd;
     }
@@ -145,17 +164,28 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
   }
 
   // Find base symbol/string table addresses
+  //
+  //
+  // linkedit_segment->vmaddr - linkedit_segment->fileoff得到是__PAGEZERO的VM Address，
+  // 而linkedit_base就是和"image list -O -F | grep fishhookTest"拿到的基地址一样，
+  // 所以，通过_dyld_get_image_vmaddr_slide计算出来的偏移slide就是ASLR。
+  // 这里获得的linkedit_base是整个image的基地址
   uintptr_t linkedit_base = (uintptr_t)slide + linkedit_segment->vmaddr - linkedit_segment->fileoff;
+  // 计算符号表在内存中的位置
   nlist_t *symtab = (nlist_t *)(linkedit_base + symtab_cmd->symoff);
+  // 计算字符串表在内存中的位置
   char *strtab = (char *)(linkedit_base + symtab_cmd->stroff);
 
   // Get indirect symbol table (array of uint32_t indices into symbol table)
+  // 获取动态符号表"表间接跳转表"在内存中的位置.
   uint32_t *indirect_symtab = (uint32_t *)(linkedit_base + dysymtab_cmd->indirectsymoff);
 
   cur = (uintptr_t)header + sizeof(mach_header_t);
   for (uint i = 0; i < header->ncmds; i++, cur += cur_seg_cmd->cmdsize) {
     cur_seg_cmd = (segment_command_t *)cur;
+    // 只处理LC_SEGMENT_64类型的load command
     if (cur_seg_cmd->cmd == LC_SEGMENT_ARCH_DEPENDENT) {
+      // 只处理"__DATA_CONST"或者"__DATA"段
       if (strcmp(cur_seg_cmd->segname, SEG_DATA) != 0 &&
           strcmp(cur_seg_cmd->segname, SEG_DATA_CONST) != 0) {
         continue;
@@ -163,9 +193,12 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
       for (uint j = 0; j < cur_seg_cmd->nsects; j++) {
         section_t *sect =
           (section_t *)(cur + sizeof(segment_command_t)) + j;
+        // 通过section的flags来查看是section是lazy还是non-Lazy
+        // __la_symbol_ptr section的flags是S_LAZY_SYMBOL_POINTERS
         if ((sect->flags & SECTION_TYPE) == S_LAZY_SYMBOL_POINTERS) {
           perform_rebinding_with_section(rebindings, sect, slide, symtab, strtab, indirect_symtab);
         }
+        // __got section的flags是S_NON_LAZY_SYMBOL_POINTERS
         if ((sect->flags & SECTION_TYPE) == S_NON_LAZY_SYMBOL_POINTERS) {
           perform_rebinding_with_section(rebindings, sect, slide, symtab, strtab, indirect_symtab);
         }
@@ -174,6 +207,7 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
   }
 }
 
+// _rebind_symbols_for_image对模块中的符号表指针进行替换。
 static void _rebind_symbols_for_image(const struct mach_header *header,
                                       intptr_t slide) {
     rebind_symbols_for_image(_rebindings_head, header, slide);
@@ -194,15 +228,23 @@ int rebind_symbols_image(void *header,
 }
 
 int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel) {
+  //
   int retval = prepend_rebindings(&_rebindings_head, rebindings, rebindings_nel);
   if (retval < 0) {
     return retval;
   }
   // If this was the first call, register callback for image additions (which is also invoked for
   // existing images, otherwise, just run on existing images
+  
+  // _rebind_symbols_for_image对模块中的符号表指针进行替换。
+    
+  // 根据_rebindings_head->next是否为空判断是不是第一次调用
   if (!_rebindings_head->next) {
+    // 第一次调用的话，调用_dyld_register_func_for_add_image注册监听方法
+    // 已经被dyld加载的image会立刻进入回调，之后的image会在dyld装载的时候出发回调_rebind_symbols_for_image
     _dyld_register_func_for_add_image(_rebind_symbols_for_image);
   } else {
+    // 遍历已经加载的image，找到所有目标函数，逐一进行hook
     uint32_t c = _dyld_image_count();
     for (uint32_t i = 0; i < c; i++) {
       _rebind_symbols_for_image(_dyld_get_image_header(i), _dyld_get_image_vmaddr_slide(i));
