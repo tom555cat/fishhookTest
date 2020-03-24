7cbbfc925ab17ccd46dd8300593b3de251c2e562
Tong Leiming <tongleiming@daling.com>
2020-03-24 12:30:27 +0800

    增加调试代码

--- fishhookTest/ViewController.m
+++ fishhookTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "fishhook/fishhook.h"
 
 @interface ViewController ()
 
@@ -17,7 +18,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    NSLog(@"我还没被绑定");
+//    NSLog(@"我不是个老司机");
+    
+//    //rebinding结构体
+//    struct rebinding nslog;
+//    nslog.name = "NSLog";
+//    nslog.replacement = myNslog;
+//    nslog.replaced = (void *)&sys_nslog;
+//    //rebinding结构体数组
+//    struct rebinding rebs[1] = {nslog};
+//    /*
+//     * 存放rebinding结构体的数组
+//     * 数组的长度
+//     */
+//    rebind_symbols(rebs, 1);
 }
+//// 函数指针
+//static void(*sys_nslog)(NSString * format, ...);
+//// 定义一个新的函数
+//void myNslog(NSString * format, ...) {
+//    format = [format stringByAppendingString:@"勾上了! \n"];
+//    // 调用原始的
+//    sys_nslog(format);
+//}
+//
+//- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+//    NSLog(@"点击了屏幕!");
+//}
 
 
 @end
